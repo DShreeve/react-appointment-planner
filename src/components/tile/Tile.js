7247d498649521f2746cf22963c03dbfb184ac45
dshreeve <ddshreeve@gmail.com>
2023-01-07 13:43:04 +0000

    Added tileList and tile functionallity

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = ({ tile }) => {
   return (
     <div className="tile-container">
-      
+      {Object.values(tile).map((value, index) => (
+        //Bold Name element (index 0)
+        <p key={index} className={`${index === 0 ? "tile-title " : ""}tile`}>
+          {value}
+        </p>
+      ))}
     </div>
   );
 };
